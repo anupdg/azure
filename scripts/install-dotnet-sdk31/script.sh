6f817fe403e9db74945663d8e21450a158df0a7a
Anup Das Gupta <anupdg@outlook.com>
2020-06-04 13:10:12 +0530

    Added scripts

---
+++ scripts/install-dotnet-sdk31/script.sh
@@ -0,0 +1,7 @@
+wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-3.1
